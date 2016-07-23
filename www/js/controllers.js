484313f168f7b6e6d6dcb80611035f62d9c17a75
Tonge <Tonge@live.cn>
2016-07-23 12:56:29 +0800

    第2讲 新建项目,架构页面,配置app.js和controllers.js - 副本

---
+++ www/js/controllers.js
@@ -0,0 +1,6 @@
+angular.module('starter.controllers', [])
+.controller('Tab1Ctrl', function($scope) {})
+.controller('Tab2Ctrl', function($scope) {})
+.controller('Tab3Ctrl', function($scope) {})
+.controller('Tab4Ctrl', function($scope) {})
+.controller('AccountCtrl', function($scope) {});
